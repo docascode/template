002135853f70858999d5a73ccda5b766ea26fd8c
Yufei Huang <yufeih@live.com>
2022-01-04 16:32:49 +0800

    Enable visual diff (#7)

--- build/start.js
+++ build/start.js
@@ -1,21 +1,26 @@
-import { join } from 'path'
-import bs from 'browser-sync'
-import { buildDist } from './esbuild.js'
-import { spawnSync } from 'child_process';
+const { join } = require('path')
+const bs = require('browser-sync')
+const { build } = require('./build.js')
+const { spawnSync } = require('child_process')
 
-buildContent()
+function start(options) {
+  buildContent()
+  
+  Promise.all([
+    build({ watch: true }),
+    serve(options)
+  ])
+}
 
-Promise.all([
-  buildDist({ watch: true }),
-  serve()
-])
-
-function serve() {
+function serve({ open, ready } = { open: true }) {
   const site = "samples/_site";
   const browserSync = bs.create('docfx')
 
   return browserSync.init({
-    open: true,
+    open,
+    callbacks: {
+      ready: ready ? (err, bs)  => ready(bs.options.get('urls').get('local')) : undefined
+    },
     files: [
       'dist',
       join(site, '**')
@@ -31,4 +36,10 @@ function buildContent() {
   const options = { stdio: 'inherit', shell: true }
   spawnSync('dotnet tool update -g docfx --version "3.0.0-*" --add-source https://docfx.pkgs.visualstudio.com/docfx/_packaging/docs-public-packages/nuget/v3/index.json', options)
   spawnSync('docfx build samples --template .', options)
+}
+
+module.exports = { start }
+
+if (require.main === module ) {
+  start()
 }
